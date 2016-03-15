2e841b510faf7d6fe77fcf962d3c29125e621a19
dbolivar <demian054m@gmail.com>
2016-03-15 01:02:45 -0430

    ultimos retoques agregar validacion de disponibilidad

--- src/java/com/apcb/business/process/APCBBusinessProcess.java
+++ src/java/com/apcb/business/process/APCBBusinessProcess.java
@@ -6,12 +6,14 @@
 
 package com.apcb.business.process;
 
-import com.apcb.paymentshandler.services.APCBPaymentsHandrerServices;
+
+import com.apcb.paymentshandler.services.PaymentsHandlerServices;
 import com.apcb.ticketshandler.services.TicketsHandlerServices;
 import com.apcb.utils.conection.ServiceGenerator;
 import com.apcb.utils.entities.Message;
 import com.apcb.utils.entities.Request;
 import com.apcb.utils.entities.Response;
+import com.apcb.utils.paymentHandler.enums.PaymentTypeEnum;
 import com.apcb.utils.ticketsHandler.enums.MessagesTypeEnum;
 import com.apcb.utils.ticketsHandler.entities.APCB_Travel;
 import com.google.gson.Gson;
@@ -28,8 +30,8 @@ public class APCBBusinessProcess {
     private Gson gson = new Gson(); 
 
     public Response ticketAvail(Request request) throws IOException, Exception  {
-        log.info("APCBBusinessProcess -> ticketReserv ini");
-        Response response = new Response();
+        log.info("APCBBusinessProcess -> ticketAvail ini");
+        Response response = new Response(request.getSesionId());
          TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
         if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
@@ -40,8 +42,8 @@ public class APCBBusinessProcess {
     }
 
     public Response ticketPrice(Request request) throws IOException, Exception  {
-        log.info("APCBBusinessProcess -> ticketReserv ini");
-        Response response = new Response();
+        log.info("APCBBusinessProcess -> ticketPrice ini");
+        Response response = new Response(request.getSesionId());
          TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
         if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
@@ -54,7 +56,7 @@ public class APCBBusinessProcess {
 
     public Response ticketReserv(Request request) throws IOException, Exception  {
         log.info("APCBBusinessProcess -> ticketReserv ini");
-        Response response = new Response();
+        Response response = new Response(request.getSesionId());
          TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
         if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
@@ -66,7 +68,7 @@ public class APCBBusinessProcess {
     
     public Response ticketDemand(Request request) throws IOException, Exception  {
         log.info("APCBBusinessProcess -> ticketDemand ini");
-        Response response = new Response();
+        Response response = new Response(request.getSesionId());
          TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
         if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
@@ -78,7 +80,7 @@ public class APCBBusinessProcess {
     
     public Response ticketCancel(Request request) throws IOException, Exception  {
         log.info("APCBBusinessProcess -> ticketCancel ini");
-        Response response = new Response();
+        Response response = new Response(request.getSesionId());
          TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
         if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
@@ -87,43 +89,66 @@ public class APCBBusinessProcess {
         response = gson.fromJson(ticketHandlerServices.ticketAirCancel(gson.toJson(request)),Response.class);  
         return response;
     }
-
     
-    public Response payCreate(Request request) throws  IOException, Exception  {
-         log.info("APCBBusinessProcess -> payCreate ini");
-        Response response = new Response();
-         APCBPaymentsHandrerServices APCBPaymentsHandrerServices = ServiceGenerator.ServiceGenerator(APCBPaymentsHandrerServices.class);
-        if (APCBPaymentsHandrerServices==null){
+     public Response ticketConsult(Request request)throws  IOException, Exception  {
+         log.info("APCBBusinessProcess -> ticketConsult ini");
+        Response response = new Response(request.getSesionId());
+         TicketsHandlerServices ticketHandlerServices = ServiceGenerator.ServiceGenerator(TicketsHandlerServices.class);
+        if (ticketHandlerServices==null){
             response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
             return response;
         }
-        response = gson.fromJson(APCBPaymentsHandrerServices.createPay(gson.toJson(request)),Response.class);  
+        response = gson.fromJson(ticketHandlerServices.ticketAirConsult(gson.toJson(request)),Response.class);  
+        return response;
+    }
+        
+    
+    public Response payCreate(Request request) throws  IOException, Exception  {
+         log.info("APCBBusinessProcess -> payCreate ini");
+        Response response = new Response(request.getSesionId());
+         PaymentsHandlerServices paymentsHandlerServices = ServiceGenerator.ServiceGenerator(PaymentsHandlerServices.class);
+        if (paymentsHandlerServices==null){
+            response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_PaymentHandler));
+            return response;
+        }
+        response = gson.fromJson(paymentsHandlerServices.createPay(gson.toJson(request)),Response.class);  
         return response;
     }
     
     public Response payComplete(Request request) throws  IOException, Exception {
         log.info("APCBBusinessProcess -> payComplete ini");
-        Response response = new Response();
-         APCBPaymentsHandrerServices APCBPaymentsHandrerServices = ServiceGenerator.ServiceGenerator(APCBPaymentsHandrerServices.class);
-        if (APCBPaymentsHandrerServices==null){
-            response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
+        Response response = new Response(request.getSesionId());
+         PaymentsHandlerServices paymentsHandlerServices = ServiceGenerator.ServiceGenerator(PaymentsHandlerServices.class);
+        if (paymentsHandlerServices==null){
+            response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_PaymentHandler));
             return response;
         }
-        response = gson.fromJson(APCBPaymentsHandrerServices.completePay(gson.toJson(request)),Response.class);  
+        response = gson.fromJson(paymentsHandlerServices.completePay(gson.toJson(request)),Response.class);  
         return response;
     }
         
-    public Response payAnnular(Request request)throws  IOException, Exception  {
+    public Response payAnnular(Request request)throws IOException, Exception  {
         log.info("APCBBusinessProcess -> payComplete ini");
-        Response response = new Response();
-         APCBPaymentsHandrerServices APCBPaymentsHandrerServices = ServiceGenerator.ServiceGenerator(APCBPaymentsHandrerServices.class);
-        if (APCBPaymentsHandrerServices==null){
-            response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_TicketsHandler));
+        Response response = new Response(request.getSesionId());
+         PaymentsHandlerServices paymentsHandlerServices = ServiceGenerator.ServiceGenerator(PaymentsHandlerServices.class);
+        if (paymentsHandlerServices==null){
+            response.setMessage(new Message(MessagesTypeEnum.ErrorAccess_PaymentHandler));
             return response;
         }
-        response = gson.fromJson(APCBPaymentsHandrerServices.annularPay(gson.toJson(request)),Response.class);  
+        response = gson.fromJson(paymentsHandlerServices.annularPay(gson.toJson(request)),Response.class);  
         return response;
     }
-        
-    
+
+    public boolean validateCostAndDisponibility(Request request) throws IOException, Exception  {
+        Response responsePrice = ticketPrice(request);
+        if(responsePrice.getMessage().getMsgCode().equals(MessagesTypeEnum.Ok.getCode())){
+            //validamos los precios
+            return true;
+        } else {
+            // no hay dispibilidad
+            return false;
+        }
+    }
+
+      
 }
--- src/java/com/apcb/business/services/APCBBusinessServices.java
+++ src/java/com/apcb/business/services/APCBBusinessServices.java
@@ -10,8 +10,8 @@ import com.apcb.business.process.APCBBusinessProcess;
 import com.apcb.utils.entities.Message;
 import com.apcb.utils.entities.Request;
 import com.apcb.utils.entities.Response;
-import com.apcb.utils.ticketsHandler.entities.APCB_Travel;
 import com.apcb.utils.ticketsHandler.enums.MessagesTypeEnum;
+import com.apcb.utils.utils.PropertiesReader;
 import com.google.gson.Gson;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -33,19 +33,25 @@ public class APCBBusinessServices {
     @WebMethod(operationName = "ticketAirAvailAndPrice")
     public String ticketAirAvailAndPrice(@WebParam(name = "request") String strRequest) {
         log.info("APCBBusinessServices -> ticketAirAvailAndPrice ini");
-        Response response = new Response();
+        Request request = new Request(strRequest); 
+        Response response;
         try {
-            Request request = new Request(strRequest); 
             APCBBusinessProcess process = new APCBBusinessProcess();
             response = process.ticketAvail(request);
             if (response.getMessage().getMsgCode().equals("200")){
-                APCB_Travel travel = response.getTravelInfo();
-                if (travel.getItinerary().length>0){
+                if (response.getTravelInfo().getItinerary().length>0){
                     request.setTravelInfo(response.getTravelInfo());
-                    response = process.ticketPrice(request); 
+                    response = process.ticketPrice(request);
+                } else {
+                    log.info("Dont call ticketPrice by itineraryList.length > 0");
+                    response = new Response(request.getSesionId());
+                    response.setMessage(new Message(MessagesTypeEnum.ErrorValidate_NoItineraryOptionsAvailable));
+                    log.error(response.getMessage().getMsgDesc());
                 }
+                
             }
         } catch (Exception e) {
+            response = new Response(request.getSesionId());
             response.setMessage(new Message(MessagesTypeEnum.Error_AplicationErrorNotHandler));
             log.error(response.getMessage().getMsgDesc(), e);
         }
@@ -56,46 +62,128 @@ public class APCBBusinessServices {
     @WebMethod(operationName = "ticketReservAndPay")
     public String ticketReservAndPay(@WebParam(name = "request") String strRequest) {
         log.info("APCBBusinessServices -> ticketReservAndPay ini");
-        Response response = new Response();
+        Request request = new Request(strRequest); 
+        Response response = new Response(request.getSesionId());
+        Response responseTicketsHandler = new Response(request.getSesionId());
+        Response responsePaymentsHandler = new Response(request.getSesionId());
+        Response responseReverse;
         try {
-            Request request = new Request(strRequest); 
+            PropertiesReader prop = new PropertiesReader("BusinessConfiguration");
             APCBBusinessProcess process = new APCBBusinessProcess();
- 
-             log.info("APCBBusinessProcess -> ticketAirAvailAndPrice ini");
+            log.info("APCBBusinessProcess -> ticketAirAvailAndPrice ini");
             boolean error = false;
-           //Reserva de voleto
-            response = process.ticketReserv(request);
-            // Crear Pago
-            if (response.getMessage().getMsgCode().equals("200")){
-                response = process.payCreate(request);
-            }
-            // Crear Voleto
-            if (response.getMessage().getMsgCode().equals("200")){
-                response = process.ticketDemand(request);
-            }
-            // Completar Pago
-            if (response.getMessage().getMsgCode().equals("200")){
-                response = process.payComplete(request);
-            }
-            // Cancelar ticket
-            if (!response.getMessage().getMsgCode().equals("200")){
-                error = true;
-                response = process.ticketCancel(request);
+            
 
-                if (response.getMessage().getMsgCode().equals("200")){
-                    // Cancelar Pago
-                    response = process.payAnnular(request);
+ 
+           //Validamo si todavia existe el boleto a reservar y si los montos son correctos
+            if (!process.validateCostAndDisponibility(request)){
+               response.setMessage(new Message(MessagesTypeEnum.ErrorValidate_CostOrDisponibility));
+               log.error(response.getMessage().getMsgDesc());
+            } else {
+                //Reserva de voleto (Kiu)
+                request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                responseTicketsHandler = process.ticketReserv(request);
+                log.info("responseTicketsHandler -> ticketReserv ->"+gson.toJson(responseTicketsHandler));
+                //request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+
+                // Consultar reserva (Kiu)
+                if (responseTicketsHandler.getMessage().getMsgCode().equals("200") && 
+                        prop.getProperty("ConsultReservBeforePay", false).equalsIgnoreCase("true")){
+                    request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                    responseTicketsHandler = process.ticketConsult(request);
+                    log.info("*************----------responseTicketsHandler -> ticketConsult ->"+gson.toJson(responseTicketsHandler));
+
                 }
+                // Crear Pago 
+                if (responseTicketsHandler.getMessage().getMsgCode().equals("200")){
+                    //request.setTravelInfo(response.getTravelInfo());
+                    responsePaymentsHandler = process.payCreate(request);
+                    log.info("*************----------responseTicketsHandler -> payCreate ->"+gson.toJson(responsePaymentsHandler));
+
+                    // Crear Voleto (Kiu)
+                    if (responseTicketsHandler.getMessage().getMsgCode().equals("200") && responsePaymentsHandler.getMessage().getMsgCode().equals("200")){
+                        request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                        request.setPayMainInfo(responsePaymentsHandler.getPayMainInfo());
+                        responseTicketsHandler = process.ticketDemand(request);
+                        log.info("*************----------responseTicketsHandler -> ticketDemand ->"+gson.toJson(responseTicketsHandler));
+
+                    }
+                    // Consultar Voleto (Kiu)
+                    if (responseTicketsHandler.getMessage().getMsgCode().equals("200") && responsePaymentsHandler.getMessage().getMsgCode().equals("200")){
+                        request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                        request.setPayMainInfo(responsePaymentsHandler.getPayMainInfo());
+                        responseTicketsHandler = process.ticketConsult(request);
+                        log.info("*************----------responseTicketsHandler -> ticketConsult ->"+gson.toJson(responseTicketsHandler));
+
+                    }
+                    // Completar Pago
+                    if (responseTicketsHandler.getMessage().getMsgCode().equals("200") && responsePaymentsHandler.getMessage().getMsgCode().equals("200")){
+                        request.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                        request.setPayMainInfo(responsePaymentsHandler.getPayMainInfo());
+                        responsePaymentsHandler = process.payComplete(request);
+                        log.info("*************----------responseTicketsHandler -> payComplete ->"+gson.toJson(responsePaymentsHandler));
+                    }
+                    // Cancelar ticket (Kiu)
+                    if (!responseTicketsHandler.getMessage().getMsgCode().equals("200") || !responsePaymentsHandler.getMessage().getMsgCode().equals("200")){
+
+                        //request = new Request();
+                        error = true;
+                        responseReverse = process.ticketCancel(request);
+                        log.info("*************----------responseTicketsHandler -> ticketCancel ->"+gson.toJson(responseReverse));
+
+                        if (responseReverse.getMessage().getMsgCode().equals("200")){
+                            // Cancelar Pago
+                            responseReverse = process.payAnnular(request);
+                            log.info("*************----------responseReverse -> payAnnular ->"+gson.toJson(responseReverse));
+                        }
+                    } else {
+                        if (prop.getProperty("CancelAllTickets", false).equalsIgnoreCase("true")){
+                            responseReverse = process.ticketCancel(request);
+                        }
+                        if (prop.getProperty("CancelAllPays", false).equalsIgnoreCase("true")){
+                            responseReverse = process.payAnnular(request);
+                        }                
+                    }
+                }
+                if (!responseTicketsHandler.getMessage().getMsgCode().equals("200")){
+                    log.error("responseTicketsHandler --> error --> "+responseTicketsHandler.getMessage().getMsgCode());
+                    response.setMessage(responseTicketsHandler.getMessage());
+                } else if (!responsePaymentsHandler.getMessage().getMsgCode().equals("200")){
+                     log.error("responsePaymentsHandler --> error --> "+responsePaymentsHandler.getMessage().getMsgCode());
+                     response.setMessage(responsePaymentsHandler.getMessage());
+                } else {
+                    response.setSesionId(request.getSesionId());
+                    response.setTravelInfo(responseTicketsHandler.getTravelInfo());
+                    response.setPayMainInfo(responsePaymentsHandler.getPayMainInfo());
+                }   
             }
-            if (error){
-                //TODO: enviar correo de error.
-            }            
             
         } catch (Exception e) {
+            response = new Response(request.getSesionId());
             response.setMessage(new Message(MessagesTypeEnum.Error_AplicationErrorNotHandler));
             log.error(response.getMessage().getMsgDesc(), e);
         }
         log.info("APCBBusinessServices -> ticketReservAndPay end");
         return gson.toJson(response);
+    }
+    
+     
+    @WebMethod(operationName = "consultReservOrTicket")
+    public String consultReservOrTicket(@WebParam(name = "request") String strRequest) {
+        log.info("APCBBusinessServices -> consultReservOrTicket ini");
+        Request request = new Request(strRequest); 
+        Response response;
+        try {
+            APCBBusinessProcess process = new APCBBusinessProcess();
+            response = process.ticketConsult(request);
+        } catch (Exception e) {
+            response = new Response(request.getSesionId());
+            response.setMessage(new Message(MessagesTypeEnum.Error_AplicationErrorNotHandler));
+            log.error(response.getMessage().getMsgDesc(), e);
+        }
+        log.info("APCBBusinessServices -> consultReservOrTicket end");
+        return gson.toJson(response);
+        
+        
     }
 }
